96f85a99fafa78f4f10321c85229d0330fa268c0
Jhoonex <162001223+Jhoonex@users.noreply.github.com>
2025-04-18 05:12:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Exibir a mensagem de boas-vindas assim que a página carregar
+  alert("Bem-vindo ao site! Aqui você encontrará vídeos educativos sobre informática.");
+});
