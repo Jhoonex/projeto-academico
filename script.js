43acfd2c32897cd13b533c42b141176cfcfd071e
Jhoonex <162001223+Jhoonex@users.noreply.github.com>
2026-06-21 02:20:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Exibir a mensagem de boas-vindas assim que a página carregar
-  alert("Bem-vindo ao site! Aqui você encontrará vídeos educativos sobre informática.");
+    const modal = document.getElementById("welcomeModal");
+    const closeBtn = document.getElementById("closeModalBtn");
+
+    // verifica se a pessoa já visitou o site antes
+    const jaVisitou = localStorage.getItem("visitouSite");
+
+    // Se não visitou, mostrar o modal
+    if (!jaVisitou) {
+        modal.style.display = "flex";
+    }
+
+    // quando a pessoa clicar no botão de fechar:
+    closeBtn.addEventListener("click", function() {
+        modal.style.display = "none"; // esconde o modal
+        
+        // grava no navegador que ela já visitou o site
+        localStorage.setItem("visitouSite", "sim");
+    });
 });
